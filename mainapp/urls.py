1a47fe8661a4095b52a6e3253d8aaa3d7f57d0b8
Max <shalyapapro@yandex.ru>
2021-06-24 17:22:52 +0300

    updated home page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from mainapp.views import PlantListView, PlantDetailView, add_plant, RegisterUserView, UserLoginView, user_logout
 
 urlpatterns = [
-    path('plant_list/', PlantListView.as_view(), name='plant_list_view'),
+    path('', PlantListView.as_view(), name='plant_list_view'),
     path('plant_detail/<int:pk>', PlantDetailView.as_view(), name='plant_detail_view'),
     path('add_plant/', add_plant, name='add_plant'),
     path('register/', RegisterUserView.as_view(), name='register'),
