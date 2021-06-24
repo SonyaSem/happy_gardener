9b07e7d4af1257aeda8cd26ac77aa6ebe04335ca
Max <shalyapapro@yandex.ru>
2021-06-24 14:29:54 +0300

    created Plant.model

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,3 +4,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=250, verbose_name="Вид растения")
+
+
+class Plant(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="plant")
+    title = models.CharField(max_length=250, verbose_name="Название растения")
+    description = models.TextField(verbose_name="Описание растения")
+    place_of_purchase = models.CharField(max_length=250, verbose_name="Место покупки")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
