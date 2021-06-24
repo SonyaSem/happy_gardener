b69622368c6da218cb9c0bffd318a06f93852125
Max <shalyapapro@yandex.ru>
2021-06-24 15:40:02 +0300

    added add_plant.view

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from mainapp.views import PlantListView, PlantDetailView
+from mainapp.views import PlantListView, PlantDetailView, add_plant
 
 urlpatterns = [
     path('plant_list/', PlantListView.as_view(), name='plant_list_view'),
     path('plant_detail/<int:pk>', PlantDetailView.as_view(), name='plant_detail_view'),
+    path('add_plant/', add_plant, name='add_plant'),
 
     ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
 
+from mainapp.forms import AddPlantForm
 from mainapp.models import Category, Plant
 
 
@@ -22,3 +23,15 @@ class PlantDetailView(ListView):
 
     def get_queryset(self):
         return Plant.objects.get(pk=self.kwargs['pk'])
+
+
+def add_plant(request):
+    if request.method == 'POST':
+        plant_form = AddPlantForm(request.POST)
+        if plant_form.is_valid():
+            plant_form.save()
+    else:
+        plant_form = AddPlantForm()
+    context = {'form': plant_form}
+
+    return render(request, 'mainapp/add_plant.html', context)
