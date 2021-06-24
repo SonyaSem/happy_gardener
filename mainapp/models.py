6291f721c0086b22e804cb479eb74938b1b73b36
Max <shalyapapro@yandex.ru>
2021-06-24 14:32:02 +0300

    updated models

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=250, verbose_name="Вид растения")
 
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = "Вид растения"
+        verbose_name_plural = "Виды растений"
+
 
 class Plant(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="plant")
@@ -12,3 +19,11 @@ class Plant(models.Model):
     description = models.TextField(verbose_name="Описание растения")
     place_of_purchase = models.CharField(max_length=250, verbose_name="Место покупки")
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = "Растение"
+        verbose_name_plural = "Растения"
+
