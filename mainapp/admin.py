f9cde6bff7be7d0d9b886383a26f3cc3e7e3ac74
Max <shalyapapro@yandex.ru>
2021-06-24 14:34:31 +0300

    added CategoryAdmin.model

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title')
+
+
+admin.site.register(Category, CategoryAdmin)
