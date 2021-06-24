1198313c33d7ff089f16215b0fa92c3c9a2844d6
Max <shalyapapro@yandex.ru>
2021-06-24 14:49:49 +0300

    updated PlantListView

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
 
+from mainapp.models import Category, Plant
+
 
 class PlantListView(ListView):
-    pass
+    model = Plant
+    template_name = 'mainapp/plant_list_view.html'
+    context_object_name = 'plants'
+
+    # выбираю только темы, относящиеся к конкретной категории
+    def get_queryset(self):
+        return Plant.objects.all()
