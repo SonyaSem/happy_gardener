a0c40cef6556537022b3f62b74e5df5726d8f88e
Даниил Лебедев <2018-00888@students.ssau.ru>
2021-06-25 15:34:31 +0400

    Подчистил код

--- mainapp/ShareService.py
+++ mainapp/ShareService.py
@@ -1,14 +1,9 @@
-import vk_api
 from instagrapi import Client
-import requests
+
 
 # для Instagram. Работает но пока только с ссылками на компе
 def share_to_instagram(login: str, password: str, image, caption: str = ""):
-    if len(caption) <= 2200:
+    if len(caption) <= 2200 and len(image) <= 10:
         bot = Client()
         bot.login(login, password)
-        #bot.photo_upload(image,caption=caption)
         bot.album_upload(image, caption=caption)
-
-
-share_to_instagram()
