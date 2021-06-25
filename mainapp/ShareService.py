a575a527a0362dadd85f51d95deb68f4786ad5fa
Даниил Лебедев <2018-00888@students.ssau.ru>
2021-06-25 15:26:51 +0400

    Insta sharring but only witl local files

--- mainapp/ShareService.py
+++ mainapp/ShareService.py
@@ -2,29 +2,13 @@ import vk_api
 from instagrapi import Client
 import requests
 
-# для Instagram. В теории должно работать, однако на практике нет
-def share_to_instagram(login: str, password: str, *images, caption: str = ""):
+# для Instagram. Работает но пока только с ссылками на компе
+def share_to_instagram(login: str, password: str, image, caption: str = ""):
     if len(caption) <= 2200:
         bot = Client()
         bot.login(login, password)
-        bot.album_upload(images, caption=caption)
+        #bot.photo_upload(image,caption=caption)
+        bot.album_upload(image, caption=caption)
 
 
-
-def share_to_vk():
-
-    #1 открыть диалог авторизации/ ha-ha не будет работать в вебе
-    app_id = "7888292"
-    authlink = "https: // oauth.vk.com / authorize?client_id={client_id}" \
-               "&redirect_url={redirect_url}" \
-               "&display={display}" \
-               "&scope={scope}" \
-               "&response_type={response_type}" \
-               "&{state}".format(
-        client_id=app_id,
-        redirect_url="gardenenr_chto-to-tam",
-        display="popup",
-        scope="wall",
-        response_type="code",
-
-    )
+share_to_instagram()
