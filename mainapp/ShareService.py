0cf24a28b4abb66a6df2e4ce665b58f3e052e3a2
Даниил Лебедев <2018-00888@students.ssau.ru>
2021-06-25 21:57:43 +0400

    Share service changed to constant

--- mainapp/ShareService.py
+++ mainapp/ShareService.py
@@ -2,6 +2,9 @@ from instagrapi import Client
 from pathlib import Path
 from typing import List
 
+INSTAGRAM_MAX_PICTURE_AMOUNT = 10
+INSTAGRAM_MAX_CAPTION_SIMBOLS_AMOUNT=2200
+
 # для Instagram. Работает но пока только с ссылками на компе
 def share_to_instagram(login: str, password: str, images:List[Path], caption: str = ""):
     """
@@ -18,8 +21,8 @@ def share_to_instagram(login: str, password: str, images:List[Path], caption: st
     caption:str
         Post caprion (max length = 2200)
     """
-    if len(caption) <= 2200 and len(images) <= 10:
-        bot =Client()
+    if len(caption) <= INSTAGRAM_MAX_CAPTION_SIMBOLS_AMOUNT and len(images) <= INSTAGRAM_MAX_PICTURE_AMOUNT:
+        bot = Client()
         bot.login(login, password)
         bot.album_upload(images, caption=caption)
 
