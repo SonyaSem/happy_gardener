0b92c8140db77b309cdb907dcf1673850cc5eaf2
Max <shalyapapro@yandex.ru>
2021-06-26 20:34:46 +0300

    updated add_plant.html

--- happy_gardener/settings.py
+++ happy_gardener/settings.py
@@ -132,5 +132,6 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
